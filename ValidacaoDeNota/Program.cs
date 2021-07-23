2dbfaff1bf9108268d3b18e7b1b8ed0d533f8253
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-07-23 17:42:54 -0300

    Add files via upload

---
+++ ValidacaoDeNota/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            double nota;
+            int qtdIgual = 0;
+            double soma = 0;
+            while (qtdIgual != 2)
+            {
+                Console.WriteLine("Informe as notas: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+
+                    if (nota >= 0 & nota <= 10)   //complete a condicional
+                    {
+                        soma = soma + nota;
+                        qtdIgual = qtdIgual + 1;                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("nota invalida");
+                    }
+            }
+            Console.WriteLine("media = {0:0.00}", (soma/2)); 
+    }
+}
